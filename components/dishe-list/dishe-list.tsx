808dad8c512f26034c12fea9c5e226e8bfd69480
Gabriel Spindola <gspindola@magnasistemas.com.br>
2022-04-16 18:38:06 -0300

    feat: Finalizando estudos com Nextjs

---
+++ components/dishe-list/dishe-list.tsx
@@ -0,0 +1,10 @@
+import Styles from "./dishe-list.module.css";
+import { Dishe } from "@/components";
+
+export function DisheList() {
+    return (
+        <div className={Styles.list}>
+            <Dishe />
+        </div>
+    )
+}
