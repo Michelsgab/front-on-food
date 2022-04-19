6fe0dc6ebf4387b6a6f8ed532a12346838f9477f
Gabriel Spindola <gspindola@magnasistemas.com.br>
2022-04-19 00:13:25 -0300

    feat: Adicionando Skeleton

---
+++ components/dish-list/dish-list.tsx
@@ -0,0 +1,25 @@
+import Styles from "./dish-list.module.css";
+import { Dish } from "@/components";
+import { api } from "@/services";
+import { useQuery } from "react-query";
+import { DishType } from "core/types";
+import { DishSkeleton } from "components/dish/dish-skeleton";
+
+export function DishList() {
+  const fetchDishes = async () => {
+    const response = await api.get<DishType[]>("/deliveries?city=sao-paulo-sp");
+    return response.data;
+  };
+
+  const { data, isFetching } = useQuery<DishType[]>("dishes", fetchDishes);
+
+  return (
+    <div className={Styles.list}>
+      {isFetching ? (
+        <DishSkeleton />
+      ) : (
+        data?.map((dish) => <Dish key={dish.id} {...dish} />)
+      )}
+    </div>
+  );
+}
